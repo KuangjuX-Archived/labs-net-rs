a2cc7669cff41dbea88d4b38ae0482ff7c9bb63e
KuangjuX <18630816527@163.com>
2021-10-22 23:40:55 +0800

    update

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -7,27 +7,33 @@ fn main() {
     let (sender, receiver) = mpsc::channel::<Vec<u8>>();
     // client.set_nonblocking(true).unwrap();
     std::thread::spawn(move || {
-        let mut buffer = [0; 1024];
-        // let receiver = receiver.clone();
-        match client.read_exact(&mut buffer) {
-            Ok(_) => {
-                let msg = String::from_utf8(buffer.to_vec()).unwrap();
-                println!("msg: {}", msg);
-            },
+        loop {
+            println!("Test");
+            let mut buffer = [0; 1024];
+            match client.read_exact(&mut buffer) {
+                Ok(_) => {
+                    let msg = String::from_utf8(buffer.to_vec()).unwrap();
+                    println!("msg: {}", msg);
+                },
 
-            Err(err) => {
-                panic!("err: {}", err);
+                Err(err) => {
+                    panic!("err: {}", err);
+                }
             }
-        }
-        match receiver.try_recv() {
-            Ok(msg) => {
-                client.write_all(&msg).unwrap();
-            },
-            Err(err) => {
-                panic!("err: {}", err);
+            match receiver.try_recv() {
+                Ok(msg) => {
+                    let mut buf = msg.clone();
+                    println!("receiver receive msg: {}", String::from_utf8(msg).unwrap());
+                    buf.resize(1024, 0);
+                    client.write_all(&buf).unwrap();
+                },
+                Err(err) => {
+                    panic!("err: {}", err);
+                }
             }
         }
     });
+
     loop {
         let mut buffer = String::new();
         println!(">>>");
